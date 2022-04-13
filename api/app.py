9179a6d4d9b22bcae7ff81824ff15ae3a6037c26
Valmir Verbani <valverbani@gmail.com>
2022-04-13 14:55:05 -0400

    ENV fix, volume fix + postgres data volumes implemented

--- api/app.py
+++ api/app.py
@@ -1,7 +1,6 @@
 # Libraries
 import os
 import requests
-from dotenv import load_dotenv, find_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,17 +8,6 @@ from flask_sqlalchemy import SQLAlchemy
 from helpers.variables import  *
 
 app= Flask(__name__)
-
-# Locate .env file in your directory
-load_dotenv(find_dotenv())
-
-# Set variables from .env file
-POSTGRES_USER= os.environ.get("POSTGRES_USER")
-POSTGRES_DB= os.environ.get("POSTGRES_DB")
-POSTGRES_PASSWORD= os.environ.get("POSTGRES_PASSWORD")
-POSTGRES_SECRET_KEY= os.environ.get("POSTGRES_SECRET_KEY")
-ZENDESK_API_KEY= os.environ.get("API_KEY")
-ZENDESK_TICKET_API= os.environ.get("ZENDESK_TICKET_API")
 
 # Connect Postgres database
 app.config['SQLALCHEMY_DATABASE_URI']= POSTGRES_SQLALCHEMY_DB_URI
@@ -37,8 +25,6 @@ def get_tickets():
 # Display information about the API calls
 @app.route('/', methods=['GET'])
 def hello():
-    print('=========================================')
-    print(POSTGRES_SQLALCHEMY_DB_URI)
     return POSTGRES_SQLALCHEMY_DB_URI
 
 if __name__ == '__main__':
--- api/helpers/variables.py
+++ api/helpers/variables.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv, find_dotenv
+
+# Locate .env file in your directory
+load_dotenv(find_dotenv())
 
 # Set variables from .env file
 POSTGRES_USER= os.environ.get("POSTGRES_USER")
@@ -6,7 +10,7 @@ POSTGRES_DB= os.environ.get("POSTGRES_DB")
 POSTGRES_PASSWORD= os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_SECRET_KEY= os.environ.get("POSTGRES_SECRET_KEY")
 POSTGRES_SQLACHEMY_TRACK_MODS= False
-POSTGRES_SQLALCHEMY_DB_URI= 'postgresql://' + POSTGRES_USER + ':' + POSTGRES_PASSWORD + '@localhost/' + POSTGRES_DB
+POSTGRES_SQLALCHEMY_DB_URI= os.environ.get("POSTGRES_SQLALCHEMY_DB_URI")
 
 ZENDESK_API_KEY= os.environ.get("API_KEY")
 ZENDESK_TICKET_API= os.environ.get("ZENDESK_TICKET_API")
